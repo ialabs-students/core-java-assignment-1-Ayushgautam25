d741d966502ac7b489b1249f158e20eb12a16f66
Ayushgautam25 <104412573+Ayushgautam25@users.noreply.github.com>
2022-06-07 18:24:28 +0530

    Add files via upload

---
+++ ProductRepository.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class ProductRepository {
+
+	private List<Product> pro;
+	ProductRepository(){
+		pro = new ArrayList<Product>();
+		Product pr1 = new Product(101,"Mustard Oil",187.00,"Grocery");
+		Product pr2 = new Product(102,"Pants",700.00,"Clothing");
+		Product pr3 = new Product(103,"Headphones",700.00,"Electronic");
+		Product pr4 = new Product(104,"mobile",32000.00,"Electronic");
+		Product pr5 = new Product(105,"Tootbrush",100.00,"Grocery");
+		Product pr6 = new Product(106,"Noodels",300.00,"Food");
+		pro.add(pr1);
+		pro.add(pr2);
+		pro.add(pr3);
+		pro.add(pr4);
+		pro.add(pr5);
+		pro.add(pr6);
+	}
+	public List<Product> getList(){
+		return pro;
+	}
+}
